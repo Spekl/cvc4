8727cfc8184af621bb650bf76fc909758bc836d8
John L. Singleton <jsinglet@gmail.com>
2015-08-30 16:09:48 -0400

    Updates to cvc4 version 1.4.0-2

--- cvc4.osx.sh
+++ cvc4.osx.sh
@@ -3,15 +3,18 @@
 ##
 ## OSX Binaries aren't easy to ship statically linked so we need to work around this...
 ##
-CWD=`pwd`
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ##
 ## This allows the cvc4 binary to find its libraries inside this package.
 ##
-export DYLD_LIBRARY_PATH=$CWD/cvc4-1.4.0/lib:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$DIR/cvc4-1.4.0/lib:$DYLD_LIBRARY_PATH
 
 ##
 ## Actually execute the real prover 
 ##
 
-./cvc4.bin "$@"
+PATH=$DIR:$PATH
+
+
+cvc4.bin "$@"
